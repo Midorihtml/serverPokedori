ec5c45a1bdf0c9663f453cbf558a1fa2b21c7af6
Guillermo Fernandez <midori@Mac-Pro.local>
2022-03-29 23:42:53 -0300

    posHeroku

--- src/db.js
+++ src/db.js
@@ -7,7 +7,8 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/d16n3ft41k5oup
+`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
